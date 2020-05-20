6757fc9add802e428eb2b9f03d34ec727c622a86
Ainsley Clark <ainsley@reddico.co.uk>
2020-05-20 13:58:23 +0100

    Update

--- public/partials/header.php
+++ public/partials/header.php
@@ -2,26 +2,30 @@
     Header
     ===================== -->
 <header class="header">
-    <!-- Logo -->
-    <figure class="header-logo">
-        <img src="" alt="">
-    </figure>
-    <!-- Hamburger -->
-    <div class="hamburger">
-        <input class="hamburger-checkbox" type="checkbox" id="hamburger-check" />
-        <label for="hamburger-check" class="hamburger-box">
-            <span class="hamburger-inner"></div>
-        </label>
+    <div class="header-cont">
+        <!-- Logo -->
+        <figure class="header-logo">
+            <img src="" alt="">
+        </figure>
+        <!-- Hamburger -->
+        <div class="hamburger">
+            <input class="hamburger-checkbox" type="checkbox" id="hamburger-check" />
+            <label for="hamburger-check" class="hamburger-box">
+                <span class="hamburger-inner"></div>
+            </label>
+        </div>
+        <!-- Navigation -->
+        <nav class="nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="#home" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
+                <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
+                <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
+                <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
+                <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
+            </ul>
+        </nav>
     </div>
-    <!-- Navigation -->
-    <nav class="nav">
-        <ul class="nav-list">
-            <li class="nav-item"><a href="#home" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
-            <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
-            <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
-            <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
-            <li class="nav-item"><a href="#home" class="nav-link">LINK</a></li>
-        </ul>
-    </nav>
 </header>
+
+<div class="container"></div>
